79717a5af6c835f5f6ed94542bf0b98d7f550c18
yan_c <1905812142@qq.com>
2024-05-10 11:28:05 +0800

    安装

---
+++ 基础/01.js
@@ -0,0 +1,4 @@
+var user = {
+    name: '哈哈'
+};
+user.age;
---
+++ 基础/011.js
@@ -0,0 +1,12 @@
+/*
+ * @Author: yan_c 1905812142@qq.com
+ * @Date: 2024-05-10 11:25:09
+ * @LastEditors: yan_c 1905812142@qq.com
+ * @LastEditTime: 2024-05-10 11:25:25
+ * @FilePath: \ytg_admind:\PersonalProject\TypeScript\ts-test\基础\011.js
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ */
+const userw = {
+    name:'哈哈'
+}
+console.log(userw.age)
