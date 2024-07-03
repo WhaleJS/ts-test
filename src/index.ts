652857c64a69ab3763c3b2226055a18456619ce6
yanchang <1905812142@qq.com>
2024-07-03 13:52:35 +0800

    提交dev分支

--- src/index.ts
+++ src/index.ts
@@ -6,6 +6,7 @@
  * @FilePath: \ytg_admind:\PersonalProject\TypeScript\ts-test\src\index.ts
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
+// dve分支新建git
 let value1:number = 1; // 基本数字类
 let value2:string = '2'; // 基本字符串类
 let value3:boolean = true; // 基本布尔类
